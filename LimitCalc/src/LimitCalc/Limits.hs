b845bed9d75fcc57440ab69152087bab40eb2f8f
vstrimaitis <vstrimaitis@gmail.com>
2018-01-14 11:21:56 +0200

    Tests refactoring.

--- LimitCalc/src/LimitCalc/Limits.hs
+++ LimitCalc/src/LimitCalc/Limits.hs
@@ -1,5 +1,10 @@
 module LimitCalc.Limits where
 
-data Point a = Finite a | PositiveInfinity | NegativeInfinity deriving Show
+data Point a = Finite a | PositiveInfinity | NegativeInfinity
 
 data Limit a = HasLimit (Point a) | NoLimit | Unknown deriving Show
+
+instance Show a => Show (Point a) where
+    show NegativeInfinity = "-inf"
+    show PositiveInfinity = "+inf"
+    show (Finite x) = show x
--- LimitCalc/test/limits/basic/BasicSpec.hs
+++ LimitCalc/test/limits/basic/BasicSpec.hs
@@ -1,137 +1,43 @@
 module Limits.Basic.BasicSpec (spec) where
 
 import Test.Hspec
-import LimitCalc
 import LimitCalc.Limits
-import LimitCalc.Expr
-import Control.Monad
+import TestsCommon
 
-enableProblematicTests = False
+tests =
+    [ Test {tInput = "sin x", tX = Finite 0,         tOutput = HasLimit (Finite 0)}
+    , Test {tInput = "sin x", tX = Finite (pi/2),    tOutput = HasLimit (Finite 1)}
+    , Test {tInput = "sin x", tX = Finite (-pi / 2), tOutput = HasLimit (Finite (-1))}
+    , Test {tInput = "sin x", tX = PositiveInfinity, tOutput = NoLimit}
+    , Test {tInput = "sin x", tX = NegativeInfinity, tOutput = NoLimit}
+    
+    , Test {tInput = "cos x", tX = Finite 0,         tOutput = HasLimit (Finite 1)}
+    , Test {tInput = "cos x", tX = Finite (pi/2),    tOutput = HasLimit (Finite 0)}
+    , Test {tInput = "cos x", tX = Finite pi,        tOutput = HasLimit (Finite (-1))}
+    , Test {tInput = "cos x", tX = PositiveInfinity, tOutput = NoLimit}
+    , Test {tInput = "cos x", tX = NegativeInfinity, tOutput = NoLimit}
+    
+    , Test {tInput = "exp x", tX = Finite 0,         tOutput = HasLimit (Finite 1)}
+    , Test {tInput = "exp x", tX = Finite 1,         tOutput = HasLimit (Finite (exp 1))}
+    , Test {tInput = "exp x", tX = PositiveInfinity, tOutput = HasLimit PositiveInfinity}
+    , Test {tInput = "exp x", tX = NegativeInfinity, tOutput = HasLimit (Finite 0)}
+    
+    , Test {tInput = "ln (1 + x)", tX = Finite (-1),      tOutput = HasLimit NegativeInfinity} -- ?
+    , Test {tInput = "ln (1 + x)", tX = Finite 0,         tOutput = HasLimit (Finite 0)}
+    , Test {tInput = "ln (1 + x)", tX = PositiveInfinity, tOutput = HasLimit PositiveInfinity}
+    
+    , Test {tInput = "atan x", tX = Finite 0,         tOutput = HasLimit (Finite 0)}
+    , Test {tInput = "atan x", tX = NegativeInfinity, tOutput = HasLimit (Finite (-pi/2))}
+    , Test {tInput = "atan x", tX = PositiveInfinity, tOutput = HasLimit (Finite (pi/2))}
+    
+    , Test {tInput = "(1 + x) ^ 1",  tX = Finite 5,         tOutput = HasLimit (Finite 6)}
+    , Test {tInput = "(1 + x) ^ 2",  tX = Finite (-2),      tOutput = HasLimit (Finite 1)}
+    , Test {tInput = "(1 + x) ^ 2",  tX = PositiveInfinity, tOutput = HasLimit PositiveInfinity}
+    , Test {tInput = "(1 + x) ^ -1", tX = PositiveInfinity, tOutput = HasLimit (Finite 0)}
+    , Test {tInput = "(1 + x) ^ 2",  tX = NegativeInfinity, tOutput = HasLimit PositiveInfinity}
+    , Test {tInput = "(1 + x) ^ -1", tX = Finite (-1),      tOutput = NoLimit}
+    -- TODO: polynomials
+    ]
 
 spec :: Spec
-spec = do
-    describe "sin x" $ do
-        let expr = Function Sin X
-        it "-> 0 as x -> 0" $ do
-            let pt = Finite 0
-            let lim = findLimit pt expr
-            lim `shouldSatisfy` almost 0
-        it "-> 1 as x -> pi / 2" $ do
-            let pt = Finite (pi / 2)
-            let lim = findLimit pt expr
-            lim `shouldSatisfy` almost 1
-        it "-> -1 as x -> -pi / 2" $ do
-            let pt = Finite (-pi / 2)
-            let lim = findLimit pt expr
-            lim `shouldSatisfy` almost (-1)
-        it "has no limit as x -> +inf" $ do
-            let pt = PositiveInfinity
-            let lim = findLimit pt expr
-            lim `shouldSatisfy` nolim
-        it "has no limit as x -> -inf" $ do
-            let pt = NegativeInfinity
-            let lim = findLimit pt expr
-            lim `shouldSatisfy` nolim
-    describe "cos x" $ do
-        let expr = Function Cos X
-        it "-> 1 as x -> 0" $ do
-            let pt = Finite 0
-            let lim = findLimit pt expr
-            lim `shouldSatisfy` almost 1
-        it "-> 0 as x -> pi / 2" $ do
-            let pt = Finite (pi / 2)
-            let lim = findLimit pt expr
-            lim `shouldSatisfy` almost 0
-        it "-> -1 as x -> pi" $ do
-            let pt = Finite pi
-            let lim = findLimit pt expr
-            lim `shouldSatisfy` almost (-1)
-        it "has no limit as x -> +inf" $ do
-            let pt = PositiveInfinity
-            let lim = findLimit pt expr
-            lim `shouldSatisfy` nolim
-        it "has no limit as x -> -inf" $ do
-            let pt = NegativeInfinity
-            let lim = findLimit pt expr
-            lim `shouldSatisfy` nolim
-    describe "exp x" $ do
-        let expr = Function Exp X
-        it "-> 1 as x -> 0" $ do
-            let pt = Finite 0
-            let lim = findLimit pt expr
-            lim `shouldSatisfy` almost 1
-        it "-> e as x -> 1" $ do
-            let pt = Finite 1
-            let lim = findLimit pt expr
-            lim `shouldSatisfy` almost (exp 1)
-        it "-> +inf as x -> +inf" $ do
-            let pt = PositiveInfinity
-            let lim = findLimit pt expr
-            lim `shouldSatisfy` pinf
-        it "-> 0 as x -> -inf" $ do
-            let pt = NegativeInfinity
-            let lim = findLimit pt expr
-            lim `shouldSatisfy` almost 0
-    describe "(1 + x) ^ a" $ do
-        let expr = Power (BinaryOp Add (Const 1) X)
-        it "-> 6 as x -> 5 with a = 1" $ do
-            let pt = Finite 5
-            let a = 1
-            let lim = findLimit pt (expr a)
-            lim `shouldSatisfy` almost 6
-        it "-> 1 as x -> -2 with a = 2" $ do
-            let pt = Finite (-2)
-            let a = 2
-            let lim = findLimit pt (expr a)
-            lim `shouldSatisfy` almost 1
-        it "-> +inf as x -> +inf with a = 2" $ do
-            let pt = PositiveInfinity
-            let a = 2
-            let lim = findLimit pt (expr a)
-            lim `shouldSatisfy` pinf
-        it "-> 0 as x -> +inf with a = -1" $ do
-            let pt = PositiveInfinity
-            let a = -1
-            let lim = findLimit pt (expr a)
-            lim `shouldSatisfy` almost 0
-        when enableProblematicTests $
-            it "-> +inf as x -> -inf with a = 2" $ do
-                let pt = NegativeInfinity
-                let a = 2
-                let lim = findLimit pt (expr a)
-                lim `shouldSatisfy` pinf
-        it "has no limit as x -> -1 with a = -1" $ do
-            let pt = Finite (-1)
-            let a = -1
-            let lim = findLimit pt (expr a)
-            lim `shouldSatisfy` nolim
-    describe "ln (1 + x)" $ do
-        let expr = Function Ln (BinaryOp Add (Const 1) X)
-        it "-> -inf as x -> -1" $ do
-            let pt = Finite (-1)
-            let lim = findLimit pt expr
-            lim `shouldSatisfy` ninf
-        it "-> 0 as x -> 0" $ do
-            let pt = Finite 0
-            let lim = findLimit pt expr
-            lim `shouldSatisfy` almost 0
-        it "-> +inf as x -> +inf" $ do
-            let pt = PositiveInfinity
-            let lim = findLimit pt expr
-            lim `shouldSatisfy` pinf
-    -- TODO: atan, polynomials
-
-
-eps = 1e-8
-
-almost expected (HasLimit (Finite actual)) = abs(actual - expected) < eps
-almost _ _ = False
-
-pinf (HasLimit PositiveInfinity) = True
-pinf _ = False
-
-ninf (HasLimit NegativeInfinity) = True
-ninf _ = False
-
-nolim NoLimit = True
-nolim _ = False
+spec = createTests tests
