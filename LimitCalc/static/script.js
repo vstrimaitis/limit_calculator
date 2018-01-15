d3c531caa426c15839eaaf95cd3e76a0108184a4
Margiris Strakšys <margirisstr@gmail.com>
2018-01-15 12:09:55 +0200

    response taken care of.

--- LimitCalc/static/script.js
+++ LimitCalc/static/script.js
@@ -18,16 +18,49 @@ function calculate(){
     let params = { //add whats missing
         method: 'POST',
         body: JSON.stringify(data),
-        headers: new Headers().append("Content-Type", "applicication/json")
+        headers: new Headers({
+            "Content-Type": "applicication/json"
+        })
     }
-
-    let answer;
 
     fetch(url, params)
     .then(resp => resp.json())
     .then(response =>{ // parse the last version of response json format
         console.log(response);
-        answer = "TODO";
+        console.log(response.result == "FunctionParseError");
+        console.log(response.errorMessage);
+        let answer = "NOT YET CALCULATED.";
+        if(response.result == "OK"){
+            if(response.hasLimit == true){
+                if(response.limit == "+inf"){
+                    answer = "+∞";
+                }else if(response.limit == "+inf"){
+                    answer = "-∞";
+                }else{
+                    answer = response.limit;
+                }
+            }else{
+                answer = "Riba neegzistuoja";
+            }
+        }else if(response.result == "FunctionParseError"){
+            answer = response.errorMessage;
+        }else if(response.result == "PointParseError"){
+            answer = response.errorMessage;
+        }else if(response.result == "UnknownLimit"){
+            answer = response.errorMessage;
+        }else if(response.result == "RanOutOfFuel"){
+            answer = response.errorMessage;
+        }else if(response.result == "UnsupportedOperation"){
+            answer = response.errorMessage;
+        }else if(response.result == "FunctionUndefined"){
+            answer = response.errorMessage;
+        }
+        
+        if(answer == "NOT YET CALCULATED."){
+            let answer = "TODO!!!!!!! SOMETHING WAS NOT CAUGHT!";
+        }
+
+
         outputField.innerHTML = answer;
     })
     /*.catch(function(error){ // print error if there is one 
